b9cbd3991d1b966247bc3cc4013c7683b010c81b
Richard Cuico <marshallrichardcuico@Richards-iMac.local>
2016-05-31 20:49:45 -0700

    Facebook signin enabled

--- SocialApp/ViewController.swift
+++ SocialApp/ViewController.swift
@@ -7,17 +7,32 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    }
+    
+    @IBAction func fbBtnPressed(sender: UIButton!) {
+        
+        let facebookLogin =  FBSDKLoginManager()
+        
+        facebookLogin.logInWithReadPermissions(["email"]) { (facebookResult: FBSDKLoginManagerLoginResult!, facebookError: NSError!) in
+            
+            if facebookError != nil {
+                print("Facebook login failed. Error \(facebookError)")
+            } else {
+                let accessToken = FBSDKAccessToken.currentAccessToken().tokenString
+                print("Successfully logged in with facebook. \(accessToken)")
+
+            }
+        }
+        
     }
 
 
