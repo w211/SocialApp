529da5f85f4238d7bb5c240fae8969a1b2afa893
Richard Cuico <marshallrichardcuico@Richards-iMac.local>
2016-06-15 14:28:18 -0700

    parsed data

--- SocialApp/FeedVC.swift
+++ SocialApp/FeedVC.swift
@@ -16,6 +16,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -25,6 +27,21 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         DataService.ds.REF_POSTS.observeEventType(FIRDataEventType.Value, withBlock: { (snapshot) in
             // This is a closure so even if it's in viewDidLoad it'll still update everytime data is changed
             print(snapshot.value)
+            
+            self.posts = []
+            if let snapshots = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                
+                for snap in snapshots {
+                    print("SNAP: \(snap)")
+                    
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, dictionary: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
     }
     
@@ -33,10 +50,14 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let post = posts[indexPath.row]
+        print(post.postDescription)
+        
         return tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostCell
     }
     
---
+++ SocialApp/Post.swift
@@ -0,0 +1,55 @@
+//
+//  Post.swift
+//  SocialApp
+//
+//  Created by Richard Cuico on 6/15/16.
+//  Copyright © 2016 Richard Cuico. All rights reserved.
+//
+
+import Foundation
+
+class Post {
+    // ! means required field and ? means not required
+    private var _postDescription: String!
+    private var _imageUrl: String?
+    private var _likes: Int!
+    private var _username: String!
+    private var _postKey: String!
+    
+    var postDescription: String {
+        return _postDescription
+    }
+    
+    var imageUrl: String? {
+        return _imageUrl
+    }
+    
+    var likes: Int {
+        return _likes
+    }
+    
+    var username: String {
+        return _username
+    }
+    
+    init(description: String, imageUrl: String?, username: String) {
+        self._postDescription = description
+        self._imageUrl = imageUrl
+        self._username = username
+    }
+    
+    init(postKey: String, dictionary: Dictionary<String, AnyObject>) {
+        self._postKey = postKey
+        
+        if let likes = dictionary["likes"] as? Int {
+            self._likes = likes
+        }
+        if let imgUrl = dictionary["imageUrl"] as? String {
+            self._imageUrl = imgUrl
+        }
+        if let desc = dictionary["description"] as? String {
+            self._postDescription = desc
+        }
+    }
+    
+}
