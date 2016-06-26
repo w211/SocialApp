a51a15eccb737e2704f7ac91f64fc12073457e57
Richard Cuico <marshallrichardcuico@Richards-iMac.local>
2016-06-26 13:14:57 -0700

    connecting IBOutLets for description and likesLabel

--- SocialApp/PostCell.swift
+++ SocialApp/PostCell.swift
@@ -14,10 +14,13 @@ class PostCell: UITableViewCell {
 
     @IBOutlet weak var profileImg: UIImageView!
     @IBOutlet weak var showcaseImg: UIImageView!
+    @IBOutlet weak var descriptionText: UITextView!
+    @IBOutlet weak var likesLbl: UILabel!
+    
+    var post: Post!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
         
         
     }
@@ -32,13 +35,14 @@ class PostCell: UITableViewCell {
         profileImg.clipsToBounds = true
         showcaseImg.clipsToBounds = true
         
-        
     }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    func configureCell(post: Post) {
+        self.post = post
+        
+        self.descriptionText.text = post.postDescription
+        self.likesLbl.text = "\(post.likes)"
+        
     }
 
 }
